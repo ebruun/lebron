8139f10ca6bfe1c024798d176db35712c039be5b
EdvardBruun <ebruun@princeton.edu>
2020-12-15 19:05:30 -0500

    clean up

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-from nba_api.stats.endpoints import playercareerstats
-#from nba_api.stats.static import players
 
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players
 
 app = Flask(__name__)
 
@@ -13,12 +13,8 @@ def get_player_total_pts(id_num):
 
     total_pts = totals_reg.get_data_frame()["PTS"][0]
 
-    # print(career.get_data_frames()[0])
-    # print(a.get_data_frame())
-    # print(a.get_data_frame()["PTS"])
-
-    # p = players.find_player_by_id(id_num)
-    # print("{} has {} points".format(p["full_name"], total_pts))
+    p = players.find_player_by_id(id_num)
+    print("{} has {} points".format(p["full_name"], total_pts))
     return total_pts
 
 
@@ -29,9 +25,9 @@ def start_page():
 
 @app.route("/", methods=["POST"])
 def reload_page():
-    a = get_player_total_pts(2544)
     b = get_player_total_pts(76003)
-
+    a = get_player_total_pts(2544)
+    
     diff = b - a
     return render_template(
         "main_page.html", value1=str(b), value2=str(a), value3=str(diff)
