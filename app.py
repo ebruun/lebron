0da6d8c4315225b1b678f2ff3cda7c4e048b5fe6
EdvardBruun <ebruun@princeton.edu>
2020-12-15 19:00:51 -0500

    added in difference calc in html

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
-from flask import Flask, render_template, request, json
-
+from flask import Flask, render_template
 from nba_api.stats.endpoints import playercareerstats
-from nba_api.stats.static import players
+#from nba_api.stats.static import players
+
 
 app = Flask(__name__)
 
+
 def get_player_total_pts(id_num):
-    p = players.find_player_by_id(id_num)
 
     career = playercareerstats.PlayerCareerStats(player_id=id_num)
     totals_reg = career.career_totals_regular_season
 
     total_pts = totals_reg.get_data_frame()["PTS"][0]
 
-    #print(career.get_data_frames()[0])
-    #print(a.get_data_frame())
-    #print(a.get_data_frame()["PTS"])
+    # print(career.get_data_frames()[0])
+    # print(a.get_data_frame())
+    # print(a.get_data_frame()["PTS"])
 
-    #print('{} has {} points'.format(p['full_name'], total_pts))
+    # p = players.find_player_by_id(id_num)
+    # print("{} has {} points".format(p["full_name"], total_pts))
     return total_pts
 
-@app.route('/')
-def start_page():
-    return render_template('main_page.html', value1 = str(0), value2 = str(0))
 
-@app.route("/", methods=['POST'])
-def reload_data():
+@app.route("/")
+def start_page():
+    return render_template("main_page.html", value1=str(0), value2=str(0))
+
+
+@app.route("/", methods=["POST"])
+def reload_page():
     a = get_player_total_pts(2544)
     b = get_player_total_pts(76003)
-    return render_template('main_page.html', value1 = str(b), value2 = str(a))
 
-if __name__=="__main__":
+    diff = b - a
+    return render_template(
+        "main_page.html", value1=str(b), value2=str(a), value3=str(diff)
+    )
+
+
+if __name__ == "__main__":
     app.run()
