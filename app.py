0c6e1026e0e483a507e9a8ac6384d46b1403c20b
EdvardBruun <ebruun@princeton.edu>
2020-12-16 18:36:37 -0500

    windmill, auto update implemented

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, Response
+
+from datetime import datetime
+import time
 
 from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.static import players
@@ -20,19 +23,48 @@ def get_player_total_pts(id_num):
 
 @app.route("/")
 def start_page():
-    return render_template("main_page.html", value1=str(0), value2=str(0))
+    #b = get_player_total_pts(76003)
+    #a = get_player_total_pts(2544)
+    b = 0
+    a = 0
+    
+    diff = b - a
+    return render_template("main_page.html", value1=str(0), value2=str(0), value3=str(diff))
 
-
+""" 
 @app.route("/", methods=["POST"])
 def reload_page():
-    b = get_player_total_pts(76003)
-    a = get_player_total_pts(2544)
+    #b = get_player_total_pts(76003)
+    #a = get_player_total_pts(2544)
+    b = 0
+    a = 0
     
     diff = b - a
     return render_template(
         "main_page.html", value1=str(b), value2=str(a), value3=str(diff)
-    )
+    ) """
+
+""" @app.route('/time_feed')
+def time_feed():
+
+    def generate():
+        
+        yield datetime.now().strftime("%Y.%m.%d|%H:%M:%S")  # return also will work
+
+    return Response(generate(), mimetype='text') """
+
+@app.route('/update_points')
+def update_points():
+    def generate2():
+        
+        b = get_player_total_pts(76003)
+        a = get_player_total_pts(2544)
+    
+        diff = b - a
+        yield str(diff)  # return also will work
+
+    return Response(generate2(), mimetype='text')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
