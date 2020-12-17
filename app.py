44c3e713f52776d5d4b990052098da33c4356508
EdvardBruun <ebruun@princeton.edu>
2020-12-16 23:02:20 -0500

    clean-up var names

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ app = Flask(__name__)
 def start_page():
     point_diff = lebron_points_countdown()
     return render_template(
-        "main_page.html", value1=str(0), value2=str(0), value3=str(point_diff)
+        "main_page.html",value=str(point_diff)
     )
 
 
 @app.route("/update_points")
 def update_points():
-    def generate2():
+    def generate():
         return lebron_points_countdown()
 
-    return Response(generate2(), mimetype="text")
+    return Response(generate(), mimetype="text")
 
 
 if __name__ == "__main__":
