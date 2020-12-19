4be05a6e7fab7201e75eceb65220cf9f7a74df59
EdvardBruun <ebruun@princeton.edu>
2020-12-18 19:27:27 -0500

    added in proxy server try

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-import time
-from datetime import datetime, timedelta
-
 import requests
 from lxml.html import fromstring
+
 from nba_api.stats.endpoints.alltimeleadersgrids import AllTimeLeadersGrids
 from nba_api.stats.endpoints.playercareerstats import PlayerCareerStats
-
 
 # from nba_api.stats.endpoints import commonplayerinfo
 # from nba_api.stats.static import players
@@ -15,7 +12,6 @@ kareem_player_id = "76003"
 lebron_player_id = "2544"
 
 cache_refresh_seconds = 5
-
 
 _cache = {}
 
@@ -52,10 +48,7 @@ def check_proxies(proxies):
             print(response.json())
             saved_proxies.append(proxy)
         except:
-            # Most free proxies will often get connection errors.
-            # You will have retry the entire request using another proxy to work.
             print("Skipping. Connnection error")
-            pass
 
     return saved_proxies
 
@@ -72,8 +65,7 @@ def get_stat_data(working_proxies):
             ).pts_leaders.get_data_frame()
             return aa
         except:
-            print("Skipping. API Connnection error")
-            pass
+            print("Skipping. Connnection error")
 
     if aa is None:
         print("dont use proxy")
@@ -82,6 +74,8 @@ def get_stat_data(working_proxies):
 
 
 def get_player_total_pts(id_num):
+
+    bb = PlayerCareerStats
     career = PlayerCareerStats(player_id=id_num)
     totals_reg = career.career_totals_regular_season
     total_pts = totals_reg.get_data_frame()["PTS"][0]
@@ -94,17 +88,17 @@ def fetch_lebron_points_countdown():
     # lebron_total_points = get_player_total_pts(id_num=lebron_player_id)
     # kareem_total_points = get_player_total_pts(id_num=kareem_player_id)
 
+    # lebron_total_points = players.find_players_by_first_name('lebron' )[0]["id"]
+    # kareem_total_points = players.find_players_by_first_name('kareem')[0]["id"]
+
     proxies = get_proxies(50)
     print("Total proxies\n", proxies, "\n")
-    time.sleep(1)
 
     working_proxies = check_proxies(proxies)
     print("\nWorking proxies\n", working_proxies, "\n")
-    time.sleep(1)
 
     api_data = get_stat_data(working_proxies)
     print(api_data)
-    time.sleep(1)
 
     kareem_total_points = api_data.loc[
         api_data["PLAYER_ID"] == int(kareem_player_id), "PTS"
@@ -116,12 +110,6 @@ def fetch_lebron_points_countdown():
     return str(max(0, kareem_total_points - lebron_total_points))
 
 
-def lebron_points_countdown():
-    cache_invalidation_time = datetime.now() - timedelta(seconds=cache_refresh_seconds)
-
-    if _cache.get("timestamp", datetime.min) < cache_invalidation_time:
-        _cache.update(
-            {"timestamp": datetime.now(), "points": fetch_lebron_points_countdown()}
-        )
-
-    return _cache["points"]
+if __name__ == "__main__":
+    a = fetch_lebron_points_countdown()
+    print(a)
