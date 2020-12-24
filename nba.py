ba613f10b8f41f3b43cb58b8fe8ef23b9446f4aa
EdvardBruun <ebruun@princeton.edu>
2020-12-23 21:50:04 -0500

    clean-up

--- nba.py
+++ nba.py
@@ -11,12 +11,12 @@ kareem_player_id = "76003"
 #kareem_static_points = get_player_static_pts(player_ID=kareem_player_id)
 kareem_static_points = 38387
 
-#lebron_player_id = "2544"
+lebron_player_id = "2544"
 #lebron_player_id = '201566' #Westbrook (playing today)
-lebron_player_id = '203507' #Giannis (playing today)
+#lebron_player_id = '203507' #Giannis (playing today)
 
-#team = 'LAL'
-team = 'MIL'
+team = 'LAL'
+#team = 'MIL'
 
 cache_refresh_seconds = 5
 _cache = {}
@@ -28,7 +28,7 @@ def check_if_game_today(player_ID,n_games,date_today):
 
     # Once the game starts it is taken off the "next games" list which is referenced here...
     # This returns an error because the 'df_today' variable is now empty
-    # once the game starts can use the "game log" endpoint to grab the game ID
+    # once the game starts it shows up in the "game log" endpoint, use this to grab the game ID
 
     #df = PlayerNextNGames(player_id = player_ID, number_of_games=n_games).next_n_games.get_data_frame()
     #df_today = df.loc[(df['GAME_DATE'] == date_today)]
@@ -101,7 +101,7 @@ def fetch_lebron_points_countdown():
     """On the road to become number 1, only Kareem to pass!"""
 
     today = date.today()
-    #diff = timedelta(0)
+
     #today = (today- diff).strftime('%b %d, %Y').upper()
     today = date.today().strftime('%Y-%m-%d')
 
@@ -121,7 +121,6 @@ def fetch_lebron_points_countdown():
 
     lebron_static_points = get_player_static_pts(player_ID=lebron_player_id)
     
-
     print("static points = {}, live points = {}\n".format(lebron_static_points, lebron_live_points))
 
     return str(max(0, kareem_static_points - (lebron_static_points + lebron_live_points)))
